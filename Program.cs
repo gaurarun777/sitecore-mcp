df2a653cb50f873f7762488c5b6f8e4cc1cb8633
Arun Sharma <gaurarun777@users.noreply.github.com>
2026-09-21 18:33:55 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// Configure all logs to go to stderr (stdout is used for the MCP protocol messages).
+builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<GraphQlTools>()
+    .WithTools<ItemServiceTools>()
+    .WithTools<PowerShellTools>()
+    .WithTools<CsdAccessTools>();
+
+await builder.Build().RunAsync();
